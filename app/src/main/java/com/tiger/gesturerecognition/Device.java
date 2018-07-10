016f80f9842ed37e4e4dcf624d13eeb8eb4bbfeb
x <x@gmail.com>
2018-07-10 15:49:39 +0800

    添加sensor识别原始code

---
+++ app/src/main/java/com/tiger/gesturerecognition/Device.java
@@ -0,0 +1,8 @@
+package com.tiger.gesturerecognition;
+
+/**
+ * Created by Tobi on 16.01.2015.
+ */
+public enum Device {
+    SMARTPHONE, SMARTWATCH
+}
---
+++ app/src/main/java/com/tiger/gesturerecognition/ExporterConstants.java
@@ -0,0 +1,10 @@
+package com.tiger.gesturerecognition;
+
+import android.os.Environment;
+
+/**
+ * Created by Tobi on 18.01.2015.
+ */
+public interface ExporterConstants {
+    public final String FOLDER_NAME = Environment.getExternalStorageDirectory().getPath() + "/MonkeyExporter/";
+}
